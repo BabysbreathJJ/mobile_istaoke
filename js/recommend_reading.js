a1beb44f46ae6b7438c771e0f689749cfb57e0b7
孙秉义 <359568366@qq.com>
2016-04-05 13:55:47 +0800

    推荐阅读

--- js/recommend_reading.js
+++ js/recommend_reading.js
@@ -22,26 +22,53 @@ $(function () {
         window.location.href='index.html?page=recommend_reading&subjectId=' + subjectId;
     }
 
-    $('.more-case').hide();
+//获取标题和内容
     var selected = 0;
-    $(".less-case-item").click(function () {
-        $(".less-case-item").removeClass('selected-case-item');
-        $(this).addClass('selected-case-item');
-        selected = $(this).index();
-        var title = $(this).text();
-        $('.case-title').text(title);
+    $.ajax({
+        
+        beforeSend: function(request){
+            request.setRequestHeader("Access-Token", "5bf8ff42582c968b74af78f148c912c1");
+        },
+        type:"GET",
+        url:"/api/extendreadings/bysubject?subjectId=" + subjectId,
+        success: function (msg){
+            $.each(msg.data, function(i, item){
+               if(i <= 6){
+                    $('<span class="case-item less-case-item">'+item.attributes.title+'</span>').click(function(){
+                        $(".case-title").text(item.attributes.title);
+                        $(".list").html(item.attributes.content);
+                        $(".less-case-item").removeClass('selected-case-item');
+                        $(this).addClass('selected-case-item');
+                        selected = $(this).index(".less-case-item");
+                    }).appendTo('.less-case-items');
+                    $('<span class="case-item more-case-item">'+item.attributes.title+'</span>').click(function(){
+                        $(".case-title").text(item.attributes.title);
+                        $(".list").html(item.attributes.content);
+                        $(".more-case-item").removeClass('selected-case-item');
+                        $(this).addClass('selected-case-item');
+                        selected = $(this).index(".more-case-item");
+                    }).appendTo('.more-case-items');
+                    if(i == 0){
+                        $(".case-title").text(item.attributes.title);
+                        $(".list").html(item.attributes.content);
+                        $(".less-case-item").addClass("selected-case-item");
 
+                    }
+               }else{
+                    $('<span class="case-item more-case-item">'+item.attributes.title+'</span>').click(function(){
+                        $(".case-title").text(item.attributes.title);
+                        $(".list").html(item.attributes.content);
+                        $(".more-case-item").removeClass('selected-case-item');
+                        $(this).addClass('selected-case-item');
+                        selected = $(this).index(".more-case-item");
+                    }).appendTo('.more-case-items');
+               }
+            });
+        }
     });
-
-    $(".more-case-item").click(function () {
-        $(".more-case-item").removeClass('selected-case-item');
-        $(this).addClass('selected-case-item');
-        selected = $(this).index();
-        var title = $(this).text();
-        $('.case-title').text(title);
-
-    });
-
+    $('.more-case').hide();
+    
+    
     $('.pack-up').click(function () {
         $(".more-case-item").removeClass('selected-case-item');
         $('.less-case-item').eq(selected).addClass('selected-case-item');
@@ -52,28 +79,11 @@ $(function () {
     $('.add-case').click(function () {
         $(".less-case-item").removeClass('selected-case-item');
         $('.more-case-item').eq(selected).addClass('selected-case-item');
-        console.log(selected);
         $('.more-case').show();
         $('.less-case').hide();
 
     });
 
     
-    //获取标题和内容
-    $.ajax({
-        
-        beforeSend: function(request){
-            request.setRequestHeader("Access-Token", "5bf8ff42582c968b74af78f148c912c1");
-        },
-        type:"GET",
-        url:"/api/extendreadings/bysubject?subjectId=" + subjectId,
-        success: function (msg){
-            $.each(msg.data, function(i, item){
-               if(i == 0){
-                    $(".case-title").text(item.attributes.title);
-                    $(".list").html(item.attributes.content);
-               }
-            });
-        }
-    });
+    
 });
